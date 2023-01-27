7e75701a413ab561bcd4fa8949efeebc69e5780f
Santhy Thomas <st20acz@herts.ac.uk>
2023-01-27 21:12:30 +0000

    Initial Commit

---
+++ MovieListingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListingApp
+//
+//  Created by Santhy Rachel Thomas on 27/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
